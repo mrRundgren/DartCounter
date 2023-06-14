ebf07bcfcfca38385a5ced4dff039b8a524f65af
anders.rundgren <anders.rundgren@elicit.se>
2023-06-14 07:33:45 +0200

    Add project files.

---
+++ DartCounter/ListHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace DartCounter;
+public static class ListHelper
+{
+    private static readonly Random rng = new Random();
+    public static void Shuffle<T>(this List<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            (list[n], list[k])=(list[k], list[n]);
+        }
+    }
+}
