a63e7fba1eaee956ed6ab9a98262a40afd41ad66
anders.rundgren <anders.rundgren@elicit.se>
2023-06-15 16:42:03 +0200

    Reworked UI to components

---
+++ DartCounter/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace DartCounter.Models;
+
+public class Player
+{
+    public string Name { get; set; } = "";
+    public int Score { get; set; } = 301;
+    public int Turns { get; set; } = 0;
+    public int Highscore { get; set; } = 0;
+}
